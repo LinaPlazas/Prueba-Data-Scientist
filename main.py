38f9766cdeeaa4768065d78dbc787750400ceeef
LinaPlazas <linalejan10@hotmail.com>
2021-07-25 14:41:16 -0500

    first deplay

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ comentarios=json.load(f)
 
 
 @app.get("/")
-async def get_comentarios(n_comentarios):
+async def get_comentarios(número_comentarios):
     comentarios1=[]
-    for i in range(int(n_comentarios)):
-        comentarios1.append(comentarios[i])
-    json.dumps(comentarios1)
-    return comentarios1
+    try:
+        for i in range(int(número_comentarios)):
+            comentarios1.append(comentarios[i])
+        json.dumps(comentarios1)
+        return comentarios1
+    except:
+        return "Número de comentarios invalido"
 
 if __name__=="__main__":
     uvicorn.run(app,host="127.0.0.1",port="8000")
