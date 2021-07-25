55ff8f9c371bb20f10d028f7ba20e5fbd82b8266
LinaPlazas <linalejan10@hotmail.com>
2021-07-25 12:00:01 -0500

    add fast api

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul 24 12:32:55 2021
-
-@author: linal
-"""
-
-
 from fastapi import FastAPI
 import uvicorn
 import json
-
+import pandas as pd
 app=FastAPI(debug=True)
+f=open("Resultados_comentarios.json",encoding='utf-8')
+comentarios=json.load(f)
 
-datos={"1":"python","2":"java","3":"JAVASCRIPT"}
+
 
 @app.get("/")
-def raiz():
-    sincodificar=json.dumps(datos)
-    return json.loads(sincodificar)
-if __name__== '__main__':
-    uvicorn.run(app,port=9090)
+async def get_comentarios(n_comentarios):
+    comentarios1=pd.DataFrame()
+    for i in range(int(n_comentarios)):
+        comentarios1=comentarios1.append(comentarios[i],ignore_index=True)
+    result = comentarios1.to_json(orient="records")
+    result = json.loads(result)
+    json.dumps(result, indent=4)
+    return result
+
+if __name__=="__main__":
+    uvicorn.run(app,host="127.0.0.1",port="8000")
